4cc211d686ec77e6ea04e9275309886ee2e373ee
Eduardo Junior Alcântara da Silva <eduardojunior562@gmail.com>
2023-07-21 18:56:55 -0300

    Iniciando o repositório

---
+++ Aplicativo do Console/SomaDeDoisNumeros/SomaDeDoisNumeros/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Digite o primeiro número:");
+string input1 = Console.ReadLine();
+int num1 = int.Parse(input1);
+Console.WriteLine("Digite o segundo número:");
+string input2 = Console.ReadLine();
+int num2 = int.Parse(input2);
+
+int soma = num1 + num2;
+
+Console.WriteLine($"A soma de {num1} e {num2} é igual a {soma}.");
