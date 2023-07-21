66b69ad370c614538a4cab2cbd09b5e6fdb66d8d
Eduardo Junior Alcântara da Silva <eduardojunior562@gmail.com>
2023-07-21 19:51:04 -0300

    Iniciando o repositório

---
+++ Aplicativo do Console/Multiplicacao/Multiplicacao/Program.cs
@@ -0,0 +1,6 @@
+﻿int a, b;
+Console.WriteLine("Digite o primeiro número:");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite o segundo número:");
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("" + a + " multiplicado " + b + " é: " + a*b);
