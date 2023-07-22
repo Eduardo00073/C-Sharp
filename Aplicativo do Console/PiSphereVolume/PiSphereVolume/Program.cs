8f4302f459582fe58ad15b62a51942d3577ab6b4
Eduardo Junior Alcântara da Silva <eduardojunior562@gmail.com>
2023-07-21 22:07:45 -0300

    Iniciando o repositório

---
+++ Aplicativo do Console/PiSphereVolume/PiSphereVolume/Program.cs
@@ -0,0 +1,15 @@
+﻿class PI
+{
+    static void Main(string[] args)
+    {
+
+        double pi, raio, volume;
+        pi = 3.14159;
+
+        raio = double.Parse(Console.ReadLine());
+        volume = (4.0 / 3) * pi * Math.Pow(raio, 3);
+
+        Console.WriteLine($"VOLUME = {volume:F3}");
+    }
+
+}
